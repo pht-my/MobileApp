3b9c07ce7788698a519d6da51e05a05ef352361c
Lenovo <Lenovo@DESKTOP-L83R32A>
2024-11-07 10:32:27 +0700

    Setup Project

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import colors from "./app/config/colors";
+import HomeScreen from "./app/screens/HomeScreen";
+import CoffeeDetailsScreen from "./app/screens/CoffeeDetailsScreen";
+import coffees from "./app/config/coffees";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: colors.dark }}>
+      <HomeScreen/>
+      {/* <CoffeeDetailsScreen coffee={coffees[2]}/> */}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
